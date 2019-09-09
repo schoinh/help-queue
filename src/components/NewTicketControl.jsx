ded362323cbb9e1a10fa9b7295f86a8608117d53
Na Hyung Choi <schoinh@gmail.com>
2019-09-09 14:28:52 -0700

    add states and UUID

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NewTicketForm from './NewTicketForm';
+import ConfirmationQuestions from './ConfirmationQuestions';
+
+class NewTicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation() {
+    this.setState({ formVisibleOnPage: true });
+  }
+
+  render() {
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation} />;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation} />;
+    }
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
+
+export default NewTicketControl;
