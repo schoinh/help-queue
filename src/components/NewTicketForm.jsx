81715ed39d52b584ace355001b78ddb073fc53e7
Student <example@example.com>
2019-09-24 13:39:33 -0700

    successfully post data to Firebase

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Moment from 'moment';
 import { connect } from 'react-redux';
-import { v4 } from 'uuid';
-import c from './../constants';
+import { addTicket } from './../actions';
 
-function NewTicketForm(props){
+function NewTicketForm(props) {
   let _names = null;
   let _location = null;
   let _issue = null;
@@ -12,16 +10,7 @@ function NewTicketForm(props){
   function handleNewTicketFormSubmission(event) {
     const { dispatch } = props;
     event.preventDefault();
-    const action = {
-      type: c.ADD_TICKET,
-      id: v4(),
-      names: _names.value,
-      location: _location.value,
-      issue: _issue.value,
-      timeOpen: new Moment(),
-      formattedWaitTime: new Moment().fromNow(true)
-    };
-    dispatch(action);
+    dispatch(addTicket(_names.value, _location.value, _issue.value));
     _names.value = '';
     _location.value = '';
     _issue.value = '';
@@ -34,16 +23,16 @@ function NewTicketForm(props){
           type='text'
           id='names'
           placeholder='Pair Names'
-          ref={(input) => {_names = input;}}/>
+          ref={(input) => { _names = input; }} />
         <input
           type='text'
           id='location'
           placeholder='Location'
-          ref={(input) => {_location = input;}}/>
+          ref={(input) => { _location = input; }} />
         <textarea
           id='issue'
           placeholder='Describe your issue.'
-          ref={(textarea) => {_issue = textarea;}}/>
+          ref={(textarea) => { _issue = textarea; }} />
         <button type='submit'>Help!</button>
       </form>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
@@ -16,7 +18,7 @@ const render = (Component) => {
   ReactDOM.render(
     <HashRouter>
       <Provider store={store}>
-        <Component/>
+        <Component />
       </Provider>
     </HashRouter>,
     document.getElementById('react-app-root')
